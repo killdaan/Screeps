411b6460cbb2efbabc1ae56b3a005841f390af6d
killdaan <killdaan@hotmail.nl>
2016-08-02 21:01:19 +0200

    garbage collection fix

--- main.js
+++ main.js
@@ -10,16 +10,18 @@ var idealBuilderAmount = 20
 
 module.exports.loop = function () {
 
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+        }
+    }
+
     if (Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity) {
         spawner.run(idealHarvesterAmount, idealUpgraderAmount, idealBuilderAmount);
     }
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.ticksToLive == 0) {
-            creep.memory = nil;
-        }
-
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
