c958a5cac5d900f93bd09063ea43ac6a325b2fa3
killdaan <killdaan@hotmail.nl>
2016-08-02 17:18:43 +0200

    autospawn

--- main.js
+++ main.js
@@ -12,27 +12,27 @@ if (Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity) {
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
     if(harvesters.length == 0) {
-        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Harvester1', {role: 'harvester'});
+        Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], 'Harvester1', {role: 'harvester'});
     }
     if(harvesters.length < idealHarvesterAmount) {
         var newNumber = harvesters.length + 1
-        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Harvester' + newNumber, {role: 'harvester'});
+        Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], 'Harvester' + newNumber, {role: 'harvester'});
     }
 
     if(upgraders.length == 0) {
-        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Upgrader1', {role: 'upgrader'});
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Upgrader1', {role: 'upgrader'});
     }
     if(upgraders.length < idealUpgraderAmount) {
         var newNumber = upgraders.length + 1
-        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Upgrader' + newNumber, {role: 'upgrader'});
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Upgrader' + newNumber, {role: 'upgrader'});
     }
 
     if(builders.length == 0) {
-        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Builder1', {role: 'builder'});
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Builder1', {role: 'builder'});
     }
     if(builders.length < idealBuilderAmount) {
         var newNumber = builders.length + 1
-        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Builder' + newNumber, {role: 'builder'});
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Builder' + newNumber, {role: 'builder'});
     }
 }
 
