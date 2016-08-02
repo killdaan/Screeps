a2b5e87bb48b740ecdf60779f905b5b02e3b288b
killdaan <killdaan@hotmail.nl>
2016-08-02 15:37:39 +0200

    more autospawn

--- main.js
+++ main.js
@@ -3,15 +3,36 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 var idealHarvesterAmount = 20
+var idealUpgraderAmount = 20
+var idealBuilderAmount = 20
 
 if (Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity) {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
     if(harvesters.length == 0) {
         Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Harvester1', {role: 'harvester'});
     }
     if(harvesters.length < idealHarvesterAmount) {
         var newNumber = harvesters.length + 1
         Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Harvester' + newNumber, {role: 'harvester'});
+    }
+
+    if(upgraders.length == 0) {
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Upgrader1', {role: 'upgrader'});
+    }
+    if(upgraders.length < idealUpgraderAmount) {
+        var newNumber = upgraders.length + 1
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Upgrader' + newNumber, {role: 'upgrader'});
+    }
+
+    if(builders.length == 0) {
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Builder1', {role: 'builder'});
+    }
+    if(builders.length < idealBuilderAmount) {
+        var newNumber = builder.length + 1
+        Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Builder' + newNumber, {role: 'builder'});
     }
 }
 
