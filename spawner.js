26a1936d486f16cdb7f647408476ee95528e41d8
killdaan <killdaan@hotmail.nl>
2016-08-02 20:44:29 +0200

    added autocomplete and another autospawn fix

--- spawner.js
+++ spawner.js
@@ -2,39 +2,37 @@ var spawner = {
 
     run: function(idealHarvesterAmount, idealUpgraderAmount, idealBuilderAmount) {
 
-        breakpoint:
-
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
         if(harvesters.length == 0 && Game.spawns['Spawn1'].spawning == null) {
             Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], 'Harvester1', {role: 'harvester'});
-            break breakpoint;
+            return spawner.run;
         }
         if(upgraders.length == 0 && Game.spawns['Spawn1'].spawning == null) {
             Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Upgrader1', {role: 'upgrader'});
-            break breakpoint;
+            return spawner.run;
         }
         if(builders.length == 0 && Game.spawns['Spawn1'].spawning == null) {
             Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Builder1', {role: 'builder'});
-            break breakpoint;
+            return spawner.run;
         }
         
-        if(harvesters.length < idealHarvesterAmount && Game.spawns['Spawn1'].spawning == null) {
+        if(harvesters.length < idealHarvesterAmount && Game.spawns['Spawn1'].spawning == null && harvesters.length <= upgraders.length && harvesters.length <= builders.length) {
             var newNumber = harvesters.length + 1
             Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], 'Harvester' + newNumber, {role: 'harvester'});
-            break breakpoint;
+            return spawner.run;
         }
-        if(upgraders.length < idealUpgraderAmount && Game.spawns['Spawn1'].spawning == null) {
+        if(upgraders.length < idealUpgraderAmount && Game.spawns['Spawn1'].spawning == null && upgraders.length <= harvesters.length && upgraders.length <= builders.length) {
             var newNumber = upgraders.length + 1
             Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Upgrader' + newNumber, {role: 'upgrader'});
-            break breakpoint;
+            return spawner.run;
         }
-        if(builders.length < idealBuilderAmount && Game.spawns['Spawn1'].spawning == null) {
+        if(builders.length < idealBuilderAmount && Game.spawns['Spawn1'].spawning == null && builders.length <= harvesters.length && builders.length <= upgraders.length) {
             var newNumber = builders.length + 1
             Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Builder' + newNumber, {role: 'builder'});
-            break breakpoint;
+            return spawner.run;
         }
     }
 }
