81b0c5173b235c0ab30ba2e6ff6563190d0ab20b
killdaan <killdaan@hotmail.nl>
2016-08-02 15:23:34 +0200

    autospawn

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
+var idealHarvesterAmount = 20
+
+if (Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity) {
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    if(harvesters.length < idealHarvesterAmount) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], 'Harvester' . (harvesters.length + 1), {role: 'harvester'});
+    }
+}
+
 module.exports.loop = function () {
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
